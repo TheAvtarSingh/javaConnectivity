9f911acff47a90f721cc1449c83572072f4ef9bc
TheAvtarSingh <singha2k2@gmail.com>
2023-03-01 12:59:25 +0530

    Commiting AddInteger Files

---
+++ AddInteger.java
@@ -0,0 +1,13 @@
+class AddInteger{
+    public static void main(String[] args){
+        System.out.println("Enter To Numbers");
+        int first = 10;
+        int second = 20;
+
+        System.out.println(first+" "+second);
+
+        // add two numbers
+        int sum = first+second;
+        System.out.println("The Sum is "+sum);
+    }
+}
